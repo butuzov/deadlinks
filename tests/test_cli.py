6a5e7ad43ffbff0718d053e109fc7013b139b010
Oleg Butuzov <butuzov@made.ua>
2019-10-01 21:31:55 +0300

    more cli tests added

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,6 +9,9 @@ TODO
 - [ ] Fails tests
 """
 
+from collections import Counter
+from random import choice
+
 import pytest
 
 from click.testing import CliRunner
@@ -21,25 +24,23 @@ def runner():
     return CliRunner()
 
 
-@pytest.mark.timeout(15)
-@pytest.mark.parametrize(
-    "external, threads, domains, pathes, failed, succeed, ignored",
-    [
-        (True, 1, [], [], 6, 21, 0),
-        (True, 2, ["google.com"], [], 6, 19, 2),
-        (True, 3, [], ["limk"], 4, 21, 2),
-        (True, 4, ["google.com"], ["limk"], 4, 19, 4),
-        (False, 5, [], [], 2, 17, 0),
-    ],
-)
-def test_cli(server, runner, external, threads, domains, pathes, failed, succeed, ignored):
+@pytest.fixture
+def url(server):
+    return "http://{}:{}".format(*server)
 
-    url = "http://{}:{}".format(*server)
-    args = [url, "-n", threads]
-    if external:
-        args.append('-e')
 
-    is_ignored = (domains or pathes)
+CLI_PARAMS = [
+    (True, 1, [], [], (6, 21, 0)),
+    (True, 2, ["google.com"], [], (6, 19, 2)),
+    (True, 3, [], ["limk"], (4, 21, 2)),
+    (True, 4, ["google.com"], ["limk"], (4, 19, 4)),
+    (True, 1, ["facebook.com"], ["404.html"], (6, 21, 0)),
+    (False, 5, [], [], (2, 17, 0)),
+]
+
+
+def make_params(external, threads, domains, pathes):
+    args = ['-n', threads]
 
     for domain in domains:
         args.append('-d')
@@ -49,13 +50,26 @@ def test_cli(server, runner, external, threads, domains, pathes, failed, succeed
         args.append('-p')
         args.append(path)
 
+    if external:
+        args.append('-e')
+
+    return args
+
+
+@pytest.mark.timeout(15)
+@pytest.mark.parametrize("external, threads, domains, pathes, results", CLI_PARAMS)
+def test_cli(url, runner, external, threads, domains, pathes, results):
+
+    # parameters
+    show_key = choice(['-s', '--show'])
+    args = [url] + make_params(external, threads, domains, pathes) + [show_key, 'none']
+
     result = runner.invoke(cli, args)
+    output = result.output.rstrip("\n").split("\n")
 
     assert result.exit_code == 0
 
-    output = result.output.split("\n")
-
-    # We testing general settings.
+    # -- SETTINGS ----------------------------------------------------------
     FIRST_LINE = 'URL=<{}>; External Cheks={}; Threads={}; Retry=0'.format(
         url,
         "On" if external else "Off",
@@ -63,8 +77,62 @@ def test_cli(server, runner, external, threads, domains, pathes, failed, succeed
     )
     assert output[0] == FIRST_LINE
 
-    # Ignored/Found/Not Found
+    # -- SHORT REPORT ----------------------------------------------------------
+    failed, succeed, ignored = results
     message = "Found {}; Not Found {}".format(succeed, failed)
-    message += "; Ignored {}".format(ignored) if is_ignored else ""
-
+    message += "; Ignored {}".format(ignored) if (domains or pathes) else ""
     assert message == output[2]
+
+
+@pytest.mark.timeout(15)
+@pytest.mark.parametrize("external, threads, domains, pathes, results", CLI_PARAMS)
+@pytest.mark.parametrize("show", ['failed', 'ok', 'ignored', 'all', 'none'])
+def test_cli_details(url, runner, external, threads, domains, pathes, results, show):
+    """ tests detailed report of default export """
+    # parameters
+    show_key = choice(['-s', '--show'])
+    args = [url] + make_params(external, threads, domains, pathes) + [show_key, show]
+
+    result = runner.invoke(cli, args)
+    output = result.output.rstrip("\n").split("\n")
+
+    # -- SHORT REPORT ----------------------------------------------------------
+    failed, succeed, ignored = results
+
+    stats = Counter()
+
+    for line in output[3:]:
+        stats[line.split(" ")[1]] += 1
+
+    if show == 'none':
+        assert len(output) == 3
+
+    if show in {'failed', 'all'}:
+        assert stats['failed'] == failed
+
+    if show in {'ok', 'all'}:
+        assert stats['succeed'] == succeed
+
+    if show in {'ignored', 'all'}:
+        assert stats['ignored'] == ignored
+
+
+def test_help(runner):
+
+    result = runner.invoke(cli, ['--help'])
+
+    assert result.exit_code == 0
+
+    for word in {'Usage', 'Examples', 'Options'}:
+        assert word in result.output
+
+
+def test_version(runner):
+
+    result = runner.invoke(cli, ['--version'])
+
+    assert result.exit_code == 0
+
+    from deadlinks.__init__ import __title__, __version__
+
+    assert result.output.rstrip("\n") == "{}: v{}".format(__title__, __version__)
