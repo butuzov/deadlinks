bc82ef3791e1e265fad4c0d62ca32049d11ad58a
Oleg Butuzov <butuzov@made.ua>
2019-10-15 17:36:04 +0300

    chores(ci&dev): bumbing python version

--- setup.py
+++ setup.py
@@ -144,6 +144,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
 
         # License
         "License :: OSI Approved :: Apache Software License",
